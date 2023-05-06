38a8eb4de077c0943fe3f356974e771688f08508
Tömördi Péter <tomordi.peter@zengo.eu>
2023-05-06 23:31:16 +0200

    add expense disable according to invalid inputs

--- moneyspendtracker/src/newexpanse/NewExpanse.tsx
+++ moneyspendtracker/src/newexpanse/NewExpanse.tsx
@@ -1,8 +1,9 @@
 import './NewExpense.css';
 import './ExpenseForm.css';
-import {useState} from "react";
+import {useRef, useState} from "react";
 function NewExpanse() {
     const [showForm, modifyFormVisibilityState] = useState(false);
+    const [canAddNew, changeCanAddNew] = useState(false);
 
     const handleAddNewClick = () => {
         modifyFormVisibilityState(true);
@@ -12,18 +13,44 @@ function NewExpanse() {
         modifyFormVisibilityState(false);
     }
 
+    const handleInputChange = () => {
+        changeCanAddNew(inputsAreValid());
+    }
+
+    const titleInputRef = useRef<HTMLInputElement>(null);
+    const amountInputRef = useRef<HTMLInputElement>(null);
+    const dateInputRef = useRef<HTMLInputElement>(null);
+
+    const inputsAreValid = () => {
+        // Check if the refs are not null
+        if (
+            titleInputRef.current &&
+            amountInputRef.current &&
+            dateInputRef.current
+        ) {
+            // Use strict equality and isNaN function
+            return (
+                titleInputRef.current.value !== "" &&
+                !isNaN(parseFloat(amountInputRef.current.value)) &&
+                dateInputRef.current.value !== ""
+            );
+        }
+        // Return false if any ref is null
+        return false;
+    };
+
     return (
       <div className="new-expense">
           { (!showForm && <button onClick={handleAddNewClick}>Add New Expanse</button>) ||
               (showForm && <div className="new-expense__controls">
                   <label>Title</label>
-                  <input/>
+                  <input ref={titleInputRef} onChange={handleInputChange}/>
                   <label>Amount</label>
-                  <input/>
+                  <input ref={amountInputRef} onChange={handleInputChange}/>
                   <label>Date</label>
-                  <input type="date"/>
+                  <input type="date" ref={dateInputRef} onChange={handleInputChange}/>
                   <button className="new-expense__actions" onClick={handleCancelClick}>Cancel</button>
-                  <button className="new-expense__actions">Add Expense</button>
+                  <button className="new-expense__actions" disabled={!canAddNew}>Add Expense</button>
               </div>)
           }
       </div>
