5bd9f5e6b09e620eb76106c0fa07746ae1d457de
Tömördi Péter <tomordi.peter@zengo.eu>
2023-05-06 20:40:59 +0200

    takaritas + new expense

---
+++ moneyspendtracker/src/newexpanse/NewExpanse.tsx
@@ -0,0 +1,10 @@
+import './NewExpense.css';
+function NewExpanse() {
+    return (
+      <div className="new-expense">
+          <button>Add New Expanse</button>
+      </div>
+    );
+}
+
+export default NewExpanse;
