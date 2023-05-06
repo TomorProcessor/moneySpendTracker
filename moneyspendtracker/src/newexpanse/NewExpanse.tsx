cadc638fa2860360c5ae264a80d1d30a9b062d0b
Tömördi Péter <tomordi.peter@zengo.eu>
2023-05-06 22:24:00 +0200

    expanse form, new expense bekotes, cancel bekotes

--- moneyspendtracker/src/newexpanse/NewExpanse.tsx
+++ moneyspendtracker/src/newexpanse/NewExpanse.tsx
@@ -1,8 +1,31 @@
 import './NewExpense.css';
+import './ExpenseForm.css';
+import {useState} from "react";
 function NewExpanse() {
+    const [showForm, modifyFormVisibilityState] = useState(false);
+
+    const handleAddNewClick = () => {
+        modifyFormVisibilityState(true);
+    }
+
+    const handleCancelClick = () => {
+        modifyFormVisibilityState(false);
+    }
+
     return (
       <div className="new-expense">
-          <button>Add New Expanse</button>
+          { (!showForm && <button onClick={handleAddNewClick}>Add New Expanse</button>) ||
+              (showForm && <div className="new-expense__controls">
+                  <label>Title</label>
+                  <input/>
+                  <label>Amount</label>
+                  <input/>
+                  <label>Date</label>
+                  <input type="date"/>
+                  <button className="new-expense__actions" onClick={handleCancelClick}>Cancel</button>
+                  <button className="new-expense__actions">Add Expense</button>
+              </div>)
+          }
       </div>
     );
 }
