225dbc79fb919b44e7e787da2ba4bfcc71be5251
Tömördi Péter <tomordi.peter@zengo.eu>
2023-05-07 19:48:24 +0200

    Provider -> invalid hook call!

--- moneyspendtracker/src/index.tsx
+++ moneyspendtracker/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import NewExpanse from "./newexpanse/NewExpanse";
+import store from "./StoreReducer";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <NewExpanse />
+      <Provider store={store}>
+        <NewExpanse />
+      </Provider>
   </React.StrictMode>
 );
