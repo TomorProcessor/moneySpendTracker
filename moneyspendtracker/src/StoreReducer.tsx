e20aa8f7cd712d0eca42c1c0d9aa7c310b6c7a69
Tömördi Péter <tomordi.peter@zengo.eu>
2023-05-09 19:58:51 +0200

    initialize store elements

--- moneyspendtracker/src/StoreReducer.tsx
+++ moneyspendtracker/src/StoreReducer.tsx
@@ -14,8 +14,32 @@ export type Action = DeleteElementAction | AddElementAction;
 // Define the interface for the state
 export interface State { elements: Array<DataElement>; }
 
+function initializeStoreElements(): Array<DataElement> {
+    const elements: Array<DataElement> = [];
+    elements.push({
+        id: 'e1',
+        title: 'Toilet Paper',
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    });
+    elements.push({ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) });
+    elements.push({
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    });
+    elements.push({
+        id: 'e4',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    });
+    return elements;
+}
+
 // Define the initial state
-const initialState: State = { elements: [], };
+const initialState: State = { elements: initializeStoreElements() };
 export interface DataElement {id: string, title: string, amount: number, date: Date}
 
 const storeReducer:Reducer<State, Action> = ( state = initialState, action )  => {
