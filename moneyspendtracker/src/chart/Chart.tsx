95e294b52a5b93cf82742273741d8bd8435defdf
Tömördi Péter <tomordi.peter@zengo.eu>
2023-05-13 20:37:20 +0200

    pass data to chartbars

--- moneyspendtracker/src/chart/Chart.tsx
+++ moneyspendtracker/src/chart/Chart.tsx
@@ -3,12 +3,13 @@ import ChartBar from "./chartbar/ChartBar";
 
 function Chart() {
     const months: Array<string> = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', "Dec"];
+    const x = 52;
 
     return (
         <div className='chart'>
             {
                 months.map(
-                  mon => (<ChartBar/>) //TODO: Mon name pass
+                  mon => (<ChartBar key={Math.random().toString(36).substring(2)} label={mon} percent={x}/>)
                 )
             }
         </div>
--- moneyspendtracker/src/chart/chartbar/ChartBar.tsx
+++ moneyspendtracker/src/chart/chartbar/ChartBar.tsx
@@ -1,12 +1,32 @@
 import './ChartBar.css';
+import {useEffect, useRef, useState} from "react";
 
-function ChartBar() {
+function ChartBar(parameter: {percent: number, label: string}) {
+    const chartBarRef = useRef<HTMLDivElement>(null);
+    const chartLabelRef = useRef<HTMLLabelElement>(null);
+    const [actualPercent, setActualPercent] = useState(0);
+
+    //run on every percent changes
+    useEffect(() => {
+        setActualPercent(parameter.percent);
+        if (chartBarRef.current) {
+            chartBarRef.current.style.height = actualPercent + '%';
+        }
+    }, [parameter.percent, actualPercent]); //still not clear, why to use both variables in dep. array
+
+    //run only once, as label won't change
+    useEffect(() => {
+        if (chartLabelRef.current) {
+            chartLabelRef.current.innerText = parameter.label;
+        }
+    });
+
     return (
         <div className='chart-bar'>
             <div className='chart-bar__inner'>
-                <div className='chart-bar__fill'></div>
+                <div className='chart-bar__fill' ref={chartBarRef}></div>
             </div>
-            <label className='chart-bar__label'>test</label>
+            <label className='chart-bar__label' ref={chartLabelRef}></label>
         </div>
     );
 }
